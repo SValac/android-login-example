37b91d5b9dd7f979251a8d0539efdc33405b50ca
SValac <shiro.valac@gmail.com>
2025-03-27 23:40:34 -0600

    added login use case to viewModel and implemete onLoginselected function with  loading

--- app/src/main/java/com/example/jetpackcomposeinstagram/login/ui/LoginViewModel.kt
+++ app/src/main/java/com/example/jetpackcomposeinstagram/login/ui/LoginViewModel.kt
@@ -1,12 +1,19 @@
 package com.example.jetpackcomposeinstagram.login.ui
 
+import android.util.Log
 import android.util.Patterns
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jetpackcomposeinstagram.login.domain.LoginUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class LoginViewModel:ViewModel() {
+
+    val loginUseCase= LoginUseCase()
+
     private val _email = MutableStateFlow<String>("")
     val email : StateFlow<String> = _email.asStateFlow()
 
@@ -16,6 +23,9 @@ class LoginViewModel:ViewModel() {
     private val _isLoginEnabled = MutableStateFlow<Boolean>(false)
     val isLoginEnabled: StateFlow<Boolean> = _isLoginEnabled.asStateFlow()
 
+    private val _isLoading = MutableStateFlow<Boolean>(false)
+    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
     fun onLoginChange(email:String, password: String){
         _email.value = email
         _password.value = password
@@ -24,5 +34,16 @@ class LoginViewModel:ViewModel() {
 
     private fun enableLogin(email: String, password: String): Boolean {
         return Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.length > 6
+    }
+
+    fun onLoginSelected(){
+        viewModelScope.launch {
+            _isLoading.value = true
+            val response = loginUseCase(email.value, password.value)
+            if (response) {
+                Log.i("Valac", "result ok")
+            }
+            _isLoading.value = false
+        }
     }
 }
